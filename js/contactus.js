4d8e2c01acfa9762dd892b764588fdd4d8556640
piranockharvey03 <piranockharvey03@gmail.com>
2025-05-27 11:37:51 +0300

    added javascript

---
+++ js/contactus.js
@@ -0,0 +1,9 @@
+
+var button=document.getElementById("js1");
+button.addEventListener("click",function() {
+alert("Message Delivered Sucessfully");
+});
+  
+  
+  
+  
---
+++ js/donate.js
@@ -0,0 +1,5 @@
+var button=document.getElementById("submit");
+button.addEventListener("click",function() {
+    alert("Information Delivered Sucessfully"+" " +"Now lets proceed to Authorization of payments");
+    
+})
